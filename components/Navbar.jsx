d28354e5648750501b2b33d33a18362a572f2f70
SJ <smwjasnosz@gmail.com>
2023-11-01 23:10:56 +0100

    Adjust animation behaviour

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => (
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
+    viewport={{ once: true, amount: 0.25 }}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
--- sections/About.jsx
+++ sections/About.jsx
@@ -12,7 +12,7 @@ const About = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
+      viewport={{ once: true, amount: 0.25 }}
       className={`${styles.innerWidth} ${styles.flexCenter} flex-col mx-auto`}
     >
       <TypingText title="| About Metaversus" textStyles="text-center" />
